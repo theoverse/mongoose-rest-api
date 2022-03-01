8d33d9265e60e18f0ee77315ff742ef8cce256a8
theoverse <theoverse@protonmail.com>
2022-03-01 08:02:45 -0600

    connected app to db

---
+++ index.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/task')
+    .then(() => console.log('Database is connected'))
+    .catch((err) => console.log(err));
