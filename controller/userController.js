7bd81f88c12ed56b4c87801dbd091b61a7dd7dc4
theoverse <theoverse@protonmail.com>
2022-03-03 13:39:14 -0600

    added task and user controllers and seperate functionalities from routes

---
+++ controller/userController.js
@@ -0,0 +1,81 @@
+const User = require('../model/User');
+
+exports.createUser = async (req, res) => {
+
+    try {
+        const user = new User(req.body)
+        await user.save()
+
+        return res.status(201).json({ success: true, user })
+    }
+    catch (e) {
+        return res.status(400).json({ success: false, message: e.message })
+    }
+}
+
+exports.getAllUsers = async (req, res) => {
+    const users = await User.find()
+    return res.json({ success: true, users })
+}
+
+exports.getSingleUser = async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        });
+    }
+
+    return res.json({ success: true, user })
+}
+
+exports.updateUser = async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        return res.json({ success: true, user })
+    }
+
+    catch (e) {
+        return res.status(400).json({
+            success: false,
+            message: e.message,
+        })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        return res.json({ success: true, user })
+    }
+
+    catch (e) {
+        return res.status(404).json({
+            success: false,
+            message: e.message,
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const colors = require('colors');
 
 const app = express();
 
